0181797035722a0e5e116f50074fd8ce86a8be1c
James200546345 <jamestanael12@gmail.com>
2025-07-27 14:15:05 +0800

    input test

--- hello.py
+++ hello.py
@@ -1,1 +1,5 @@
 print("Hello, world!")
+
+name = input("What's your name? ")  # This line waits for the user to enter their name.
+
+print(f"Hello, {name}!")  # This line prints a greeting using the name provided by the user.
