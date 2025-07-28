72abcf07bcb8db61eab76ae3e1cb91cfa475e0a5
James200546345 <jamestanael12@gmail.com>
2025-07-28 22:01:33 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,4 +2,4 @@ print("Hello, world!")
 
 name = input("What's your name? ")  # This line waits for the user to enter their name.
 
-print(f"Hello, {name}!")  # This line prints a greeting using the name provided by the user.
+print(f"Hello, {name}!")  # This line prints a greeting using the name provided by the us
